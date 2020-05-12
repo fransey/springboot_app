a3986f4515f2116d9985e2f4507f17c5992d7788
Francesca <57770810+fransey@users.noreply.github.com>
2020-05-12 19:24:45 +0200

    Update CentralizedConfigurationClientApplication.java

--- src/main/java/com/example/centralizedconfigurationclient/CentralizedConfigurationClientApplication.java
+++ src/main/java/com/example/centralizedconfigurationclient/CentralizedConfigurationClientApplication.java
@@ -18,11 +18,10 @@ public class CentralizedConfigurationClientApplication {
 
 }
 
-
 @RefreshScope
 @RestController
 class MessageRestController {
-    @Value("<!DOCTYPE html> <html> <head><meta http-equiv=\"refresh\" content=\"5\" ></head> <body bgcolor=#\"${color: E6E6FA}\"> <h1> Spring Cloud App - My App</h1> <b>Name: </b> ${spring.application.name} <br><b>Version: </b> ${spring.application.version} <br><b>Profile: </b> ${spring.profiles.active} <br><b>Message: </b> ${message: default message} </body> </html>")
+    @Value("<!DOCTYPE html> <html> <head><meta http-equiv=\"refresh\" content=\"20\" ></head> <body bgcolor=#\"${color: E6E6FA}\"> <h1> This is a Spring boot Application </h1> <b>Name: </b> ${spring.application.name} <br><b>Version: </b> ${spring.application.version} <br><b>Profile: </b> ${spring.profiles.active} <br><b>Message: </b> ${message: default message} </body> </html>")
     private String message;
     private String color;
 
@@ -33,25 +32,5 @@ class MessageRestController {
 
     String getColor() {
         return this.color;
-    }
-
-    public void testMethod()
-    {
-        String test01 = "test01";
-        String test02 = "test02";
-        String test03 = "test03";
-        String test04 = "test04";
-        String test05 = "test05";
-        String test06 = "test06";
-        String test07 = "test07";
-        String test08 = "test08";
-        String test09 = "test09";
-        String test10 = "test10";
-        String test11 = "test11";
-        String test12 = "test12";
-        String test13 = "test13";
-        String test14 = "test14";
-        String test15 = "test15";
-        String test16 = "test16";
     }
 }
